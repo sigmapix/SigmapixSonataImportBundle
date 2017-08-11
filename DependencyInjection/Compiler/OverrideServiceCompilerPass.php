cea3dc2b64580d00bdf6f72d866c81c038f9125e
Alexandre MOOTASSEM <alexandre.mootassem@gmail.com>
2017-08-11 19:10:40 +0200

    Add headers to exporter even when data is empty

---
+++ DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sigmapix\Sonata\ImportBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OverrideServiceCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('sonata.admin.exporter');
+        $definition->setClass('Sigmapix\Sonata\ImportBundle\Export\Exporter');    }
+}
--- SigmapixSonataImportBundle.php
+++ SigmapixSonataImportBundle.php
@@ -2,10 +2,20 @@
 
 namespace Sigmapix\Sonata\ImportBundle;
 
+use Sigmapix\Sonata\ImportBundle\DependencyInjection\Compiler\OverrideServiceCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SigmapixSonataImportBundle extends Bundle
 {
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new OverrideServiceCompilerPass());
+    }
+
     /**
      * {@inheritdoc}
      */
