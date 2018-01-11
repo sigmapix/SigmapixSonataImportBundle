36a75035e73b590ab578606aa78fb2b4c96f582a
Alex Mootassem <alexandre.mootassem@gmail.com>
2018-01-11 15:56:16 +0100

    Applied fixes from FlintCI

--- Admin/ImportableAdminTrait.php
+++ Admin/ImportableAdminTrait.php
@@ -2,20 +2,20 @@
 
 namespace Sigmapix\Sonata\ImportBundle\Admin;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Port\Steps\Step\ValueConverterStep;
+use Port\Steps\StepAggregator;
+use Port\ValueConverter\DateTimeValueConverter;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
-use Symfony\Component\Form\Form;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Port\Steps\StepAggregator;
-use Port\Steps\Step\ValueConverterStep;
-use Port\ValueConverter\DateTimeValueConverter;
 
 trait ImportableAdminTrait
 {
@@ -23,15 +23,6 @@ trait ImportableAdminTrait
      * @var Form
      */
     private $importForm;
-
-    protected function configureRoutes(RouteCollection $collection)
-    {
-        /* @var AbstractAdmin $this */
-        $collection
-                ->add('import', 'upload/{fileName}')
-                ->add('upload')
-        ;
-    }
 
     /**
      * {@inheritdoc}
@@ -42,21 +33,12 @@ trait ImportableAdminTrait
         $this->formOptions['data_class'] = $this->getClass();
 
         $formBuilder = $this->getFormContractor()->getFormBuilder(
-            "import_form", $this->formOptions
+            'import_form', $this->formOptions
         );
 
         $this->defineImportFormBuilder($formBuilder, $headers);
 
         return $formBuilder;
-    }
-
-    protected function buildImportForm(array $headers)
-    {
-        if ($this->importForm) {
-            return;
-        }
-
-        $this->importForm = $this->getImportFormBuilder($headers)->getForm();
     }
 
     // todo: use defineFormBuilder for import Action and upload Action
@@ -69,31 +51,33 @@ trait ImportableAdminTrait
         $trans = $this->getConfigurationPool()->getContainer()->get('translator');
 
         $oldValue = ini_get('mbstring.substitute_character');
-        ini_set('mbstring.substitute_character', "none");
+        ini_set('mbstring.substitute_character', 'none');
         foreach ($formBuilder as $field) {
             /* @var FormBuilder $field */
-            if ($field->getType()->getInnerType() instanceof EntityType) continue;
+            if ($field->getType()->getInnerType() instanceof EntityType) {
+                continue;
+            }
             if ($field->getPropertyPath() && $field->getPropertyPath()->getLength() > 1) {
                 $mapper->add(
-                    (string)$field->getPropertyPath(), 'choice', array(
-                    'choices' => $headers, 
-                    'data' => $this->nearest($field->getOption("label"), $headers, $trans), 
+                    (string) $field->getPropertyPath(), 'choice', [
+                    'choices' => $headers,
+                    'data' => $this->nearest($field->getOption('label'), $headers, $trans),
                     'mapped' => false,
-                    'label' => $field->getOption("label")
-                ));
-            } else if ((string)$field->getPropertyPath() === 'id') {
-                $mapper->add($field->getName(), 'choice', array(
-                    'choices' => $headers, 
-                    'data' => $this->nearest($field->getOption("label"), $headers, $trans),
+                    'label' => $field->getOption('label'),
+                ]);
+            } elseif ('id' === (string) $field->getPropertyPath()) {
+                $mapper->add($field->getName(), 'choice', [
+                    'choices' => $headers,
+                    'data' => $this->nearest($field->getOption('label'), $headers, $trans),
                     'mapped' => false,
-                    'label' => $field->getOption("label")
-                ));
+                    'label' => $field->getOption('label'),
+                ]);
             } else {
-                $mapper->add($field->getName(), 'choice', array(
-                    'choices' => $headers, 
-                    'data' => $this->nearest($field->getOption("label"), $headers, $trans),                    
-                    'label' => $field->getOption("label")
-                ));
+                $mapper->add($field->getName(), 'choice', [
+                    'choices' => $headers,
+                    'data' => $this->nearest($field->getOption('label'), $headers, $trans),
+                    'label' => $field->getOption('label'),
+                ]);
             }
         }
         ini_set('mbstring.substitute_character', $oldValue);
@@ -106,9 +90,9 @@ trait ImportableAdminTrait
     {
         $buttonList = parent::configureActionButtons($admin, $object);
 
-        $buttonList['import'] = array(
+        $buttonList['import'] = [
             'template' => 'SigmapixSonataImportBundle:Button:import_button.html.twig',
-        );
+        ];
 
         return $buttonList;
     }
@@ -121,44 +105,18 @@ trait ImportableAdminTrait
         return $this->importForm;
     }
 
-    private function nearest($input, $words, $trans)
-    {
-        // TODO $input should be the $field, to try both 'name' and 'propertyPath' attributes
-        $closest = "";
-        $shortest = -1;
-
-        foreach ($words as $word) {
-            $wordASCII = mb_convert_encoding($word, 'ASCII');
-            $lev = levenshtein($input, $wordASCII);
-            $levCase = levenshtein(strtolower($input), strtolower($wordASCII));
-            $levTrans = levenshtein($trans->trans($input), $wordASCII);
-            $lev = min([$lev, $levCase, $levTrans]);
-            if ($lev === 0) {
-                $closest = $word;
-                $shortest = 0;
-                break;
-            }
-            if ($lev <= $shortest || $shortest < 0) {
-                $closest  = $word;
-                $shortest = $lev;
-            }
-        }
-
-        return $closest;
-    }
-
     public function configureImportSteps(StepAggregator $workflow)
-    {   
+    {
         $dateTimeFields = [];
         foreach ($this->importForm as $f) {
-            
+
             /** @var Form $f */
             /** @var FieldDescription $fieldOptions */
-            $fieldOptions = $f->getConfig()->getOption('sonata_field_description');            
-            if ($fieldOptions && ($fieldOptions->getMappingType() === 'datetime'|| $fieldOptions->getMappingType() === 'date')) {
+            $fieldOptions = $f->getConfig()->getOption('sonata_field_description');
+            if ($fieldOptions && ('datetime' === $fieldOptions->getMappingType() || 'date' === $fieldOptions->getMappingType())) {
                 $dateTimeFields[] = $f->getName();
-            }           
-        }    
+            }
+        }
         $converter = new DateTimeValueConverter('d/m/Y');
         $converterStep = new ValueConverterStep();
         foreach ($dateTimeFields as $dateTimeField) {
@@ -166,7 +124,6 @@ trait ImportableAdminTrait
         }
 
         $workflow->addStep($converterStep);
-        
     }
 
     /**
@@ -174,7 +131,7 @@ trait ImportableAdminTrait
      * It's called from importAction.
      *
      * @param Request $request
-     * @param Form $form
+     * @param Form    $form
      *
      * @return Response|null
      */
@@ -186,14 +143,58 @@ trait ImportableAdminTrait
      * This method can be overloaded in your Admin service.
      * It's called from importAction.
      *
-     * @param Request $request
-     * @param UploadedFile   $file
-     * @param Form $form
-     * @param mixed $results
+     * @param Request      $request
+     * @param UploadedFile $file
+     * @param Form         $form
+     * @param mixed        $results
      *
      * @return Response|null
      */
     public function postImport(Request $request, UploadedFile $file, Form $form, $results)
     {
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        /* @var AbstractAdmin $this */
+        $collection
+                ->add('import', 'upload/{fileName}')
+                ->add('upload')
+        ;
+    }
+
+    protected function buildImportForm(array $headers)
+    {
+        if ($this->importForm) {
+            return;
+        }
+
+        $this->importForm = $this->getImportFormBuilder($headers)->getForm();
+    }
+
+    private function nearest($input, $words, $trans)
+    {
+        // TODO $input should be the $field, to try both 'name' and 'propertyPath' attributes
+        $closest = '';
+        $shortest = -1;
+
+        foreach ($words as $word) {
+            $wordASCII = mb_convert_encoding($word, 'ASCII');
+            $lev = levenshtein($input, $wordASCII);
+            $levCase = levenshtein(strtolower($input), strtolower($wordASCII));
+            $levTrans = levenshtein($trans->trans($input), $wordASCII);
+            $lev = min([$lev, $levCase, $levTrans]);
+            if (0 === $lev) {
+                $closest = $word;
+                $shortest = 0;
+                break;
+            }
+            if ($lev <= $shortest || $shortest < 0) {
+                $closest = $word;
+                $shortest = $lev;
+            }
+        }
+
+        return $closest;
     }
 }
--- Service/ImportService.php
+++ Service/ImportService.php
@@ -2,31 +2,24 @@
 
 namespace Sigmapix\Sonata\ImportBundle\Service;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Port\Csv\CsvReader;
 use Port\Doctrine\DoctrineWriter;
 use Port\Excel\ExcelReader;
-use Port\Steps\Step\ValueConverterStep;
 use Port\Steps\StepAggregator;
-use Port\ValueConverter\DateTimeValueConverter;
-use Doctrine\ORM\EntityManagerInterface;
-use Sonata\DoctrineORMAdminBundle\Admin\FieldDescription;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Port\Steps\Step\ConverterStep;
-use AppBundle\Entity\Residence;
-use AppBundle\Entity\Family;
-use AppBUndle\Entity\Building;
 
 final class ImportService
 {
     /**
      * @var EntityManagerInterface
      */
-    protected $em;
+    private $em;
 
     /**
      * @var SessionInterface
@@ -45,10 +38,11 @@ final class ImportService
 
     /**
      * ImportService constructor.
+     *
      * @param EntityManagerInterface $em
      * @param $session
      * @param ContainerInterface $container
-     * @param string $doctrineWriterClass
+     * @param string             $doctrineWriterClass
      */
     public function __construct(EntityManagerInterface $em, $session, ContainerInterface $container, string $doctrineWriterClass)
     {
@@ -60,39 +54,44 @@ final class ImportService
 
     /**
      * @param UploadedFile $file
+     *
      * @return array
      */
     public function getHeaders(UploadedFile $file)
     {
         $reader = $this->getReader($file);
-        $columnHeaders = array_filter($reader->getColumnHeaders(),function($h){return !is_null($h);} );
-        $columnHeaders = array_map(function($h){return trim($h);}, $columnHeaders);
+        $columnHeaders = array_filter($reader->getColumnHeaders(), function ($h) {return null !== $h; });
+        $columnHeaders = array_map(function ($h) {return trim($h); }, $columnHeaders);
         $headers = array_flip($columnHeaders);
-        array_walk($headers, function(&$v, $k) use ($headers) { $v = $k; });
+        array_walk($headers, function (&$v, $k) use ($headers) { $v = $k; });
+
         return $headers;
     }
 
     /**
-     * @param UploadedFile $file
-     * @param Form $form
+     * @param UploadedFile   $file
+     * @param Form           $form
      * @param AdminInterface $admin
+     *
      * @return mixed
      */
     public function import(UploadedFile $file, Form $form, AdminInterface $admin)
     {
-        
         $mapping = [];
         foreach ($form as $f) {
-            if ($f instanceof SubmitButton) continue;
+            if ($f instanceof SubmitButton) {
+                continue;
+            }
             $mapping[$f->getName()] = $f->getNormData();
         }
 
         $reader = $this->getReader($file);
 
         // Replace columnsHeader names with entity field name in our $mapping
-        $columnHeaders = array_map(function($h) use ($mapping) {
+        $columnHeaders = array_map(function ($h) use ($mapping) {
             $k = array_search(trim($h), (array) $mapping, true);
-            return ($k === false ? $h : $k);
+
+            return false === $k ? $h : $k;
         }, $reader->getColumnHeaders());
         $reader->setColumnHeaders($columnHeaders);
 
@@ -102,24 +101,48 @@ final class ImportService
             $writer->setContainer($this->container);
         }
         $writer->disableTruncate();
-        
+
         $workflow = new StepAggregator($reader);
-        $workflow->addWriter($writer);         
-        $admin->configureImportSteps($workflow);   
-        
+        $workflow->addWriter($writer);
+        $admin->configureImportSteps($workflow);
+
         $result = $workflow->process();
+
         return $result;
     }
 
     /**
+     * Get session.
+     *
+     * @return
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    /**
+     * Set session.
+     *
+     * @return $this
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+
+        return $this;
+    }
+
+    /**
      * @param UploadedFile $file
+     *
      * @return CsvReader|ExcelReader
      */
     private function getReader(UploadedFile $file)
     {
         $pathFile = $file->getRealPath();
         $fileExtension = $file->guessExtension();
-        $excelExtensions = array('xls', 'xlsx', 'zip');
+        $excelExtensions = ['xls', 'xlsx', 'zip'];
 
         if (in_array($fileExtension, $excelExtensions)) {
             $reader = new ExcelReader(new \SplFileObject($pathFile), 0, 0);
@@ -129,24 +152,5 @@ final class ImportService
         }
 
         return $reader;
-    }
-
-    /**
-    * Get session
-    * @return  
-    */
-    public function getSession()
-    {
-        return $this->session;
-    }
-    
-    /**
-    * Set session
-    * @return $this
-    */
-    public function setSession($session)
-    {
-        $this->session = $session;
-        return $this;
     }
 }
